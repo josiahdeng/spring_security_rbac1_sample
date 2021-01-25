d29b0375e136bfe9d0b13a27697293061cf0febd
lucian <17607003651@163.com>
2021-01-25 10:34:52 +0800

    Initial commit

---
+++ src/main/java/com/lucian/spring_security_rbac1_sample/SpringSecurityRbac1SampleApplication.java
@@ -0,0 +1,13 @@
+package com.lucian.spring_security_rbac1_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRbac1SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityRbac1SampleApplication.class, args);
+    }
+
+}
